d48bd48a114a1e1a0798c5207e69ed6ab1140358
Carinalak <carina.lakosil@gmail.com>
2024-08-10 08:35:23 +0200

    :memo: Added information in Om

--- src/components/Om.vue
+++ src/components/Om.vue
@@ -10,30 +10,93 @@
 
 
 
-<section class="main-container">
+<section class="main-container-align-top">
     <article class="text-content">
         <h2>Om</h2>
-        <div>
+        <p>
             Hej! Mitt namn är 
-            Carina och jag bor i 
+            Carina och jag bor på Ekerö nära 
             Stockholm. Jag studerar 
-            till Front End Utvecklare på MedieInstitutet i Stockholm 
+            till Front End Utvecklare på MedieInstitutet
             och är på mitt andra år
-            nu. På den här sidan 
-            hittar du några av de 
-            projekt jag gjort och annat intressant.
-            Jag letar just nu efter LIA-plats det får gärna vara på distans eller hybrid, då jag är 
-            väldigt duktig på att få mycket gjort när jag jobbar hemifrån. Om du är arbetsgivare och vill ha mitt CV, 
+            nu. På den här sidan delar jag med mig av några av de 
+            projekt jag gjort, både under studietiden, men även utanför.
+            Jag letar just nu efter LIA-plats med början denna höst. Om du är arbetsgivare och vill ha mitt CV, 
             maila mig.
-        </div>
+            Vår hösttermin 2024 börjar vi med att lära oss React.
+        </p>
         
         <div class="citat2">Enjoy</div>
     </article>
-
     <article>
         <img src="../assets/img/CarinaFoto24.png" alt="En bild på en glad tjej i halvlångt brunt hår och glasögon.">
     </article>
 
+</section>
+<section class="main-container">
+    <article> 
+        <div class="image1">Lägg till en bild här 1</div>
+    </article>
+    <article class="text-content">
+        <h2>Mina kunskaper</h2>
+        <div><ul class="kunskaps-lista">
+            <li>
+                HTML
+            </li>
+            <li>
+                CSS, SASS
+            </li>
+            <li>
+                JavaScript
+            </li>
+            <li>
+                Typescript
+            </li>
+            <li>
+                Vue
+            </li>
+            <li>
+                Adobe Photoshop
+            </li>
+            <li>
+                Adobe Illustrator
+            </li>
+            <li>
+                SVG-animation
+            </li>
+            <li>
+                Figma
+            </li>
+            <li>
+                Adobe Premiere Elements
+            </li>
+            <li>
+                Ux-design
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+            <li>
+                
+            </li>
+
+
+        </ul></div>
+        <article> 
+        <div class="image2">Lägg till en bild här 2</div>
+    </article>
+    </article>
+</section>
+<section >
+    <article class="main-container-align-top-left">
+
+        
+        
+    </article>
+    <article></article>
 </section>
 <section>
         <div class="citat1">Enjoy</div>
@@ -66,7 +129,18 @@ img {
         font-family: "Caveat", cursive;
         font-size: 2.6rem;
 
-    }
+}
+.kunskaps-lista {
+    margin-left: 0;
+    padding-left: 0;
+}
+.image1 {
+    display: none;
+}
+.image2 {
+    display: block;
+    padding-top: 40px;
+}
 /*----------------- TABLET -----------------------*/
 @media screen and (min-width: 640px) {
     img {
@@ -84,6 +158,12 @@ img {
         font-size: 3.5rem;
         text-align: center;
         padding-top: 80px;
+    }
+    .image1 {
+    display: block;
+    }
+    .image2 {
+        display: none;
     }
 }
 
